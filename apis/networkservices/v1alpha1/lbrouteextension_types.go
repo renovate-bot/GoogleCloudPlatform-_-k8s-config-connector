28d03122b57f24c655d75395f8685aa25a4f0caf
Katriel Traum <katriel@google.com>
2026-02-26 13:32:46 +0000

    reformat docstrings and clarify WasmPluginRef support

--- apis/networkservices/v1alpha1/lbrouteextension_types.go
+++ apis/networkservices/v1alpha1/lbrouteextension_types.go
@@ -92,11 +92,11 @@ type NetworkServicesLBRouteExtensionObservedState struct {
 // +kcc:proto=google.cloud.networkservices.v1.ExtensionChain
 type ExtensionChain struct {
 	// Required. The name for this extension chain.
-	//  The name is logged as part of the HTTP request logs.
-	//  The name must conform with RFC-1034, is restricted to lower-cased letters,
-	//  numbers and hyphens, and can have a maximum length of 63 characters.
-	//  Additionally, the first character must be a letter and the last a letter or
-	//  a number.
+	// The name is logged as part of the HTTP request logs.
+	// The name must conform with RFC-1034, is restricted to lower-cased letters,
+	// numbers and hyphens, and can have a maximum length of 63 characters.
+	// Additionally, the first character must be a letter and the last a letter or
+	// a number.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.name
 	// +required
 	Name *string `json:"name,omitempty"`
@@ -107,11 +107,11 @@ type ExtensionChain struct {
 	MatchCondition *ExtensionChain_MatchCondition `json:"matchCondition,omitempty"`
 
 	// Required. A set of extensions to execute for the matching request.
-	//  At least one extension is required.
-	//  Up to 3 extensions can be defined for each extension chain
-	//  for `LBTrafficExtension` resource.
-	//  `LBRouteExtension` and `LBEdgeExtension` chains are limited to 1 extension
-	//  per extension chain.
+	// At least one extension is required.
+	// Up to 3 extensions can be defined for each extension chain
+	// for `LBTrafficExtension` resource.
+	// `LBRouteExtension` and `LBEdgeExtension` chains are limited to 1 extension
+	// per extension chain.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.extensions
 	// +required
 	Extensions []ExtensionChain_Extension `json:"extensions,omitempty"`
@@ -120,10 +120,10 @@ type ExtensionChain struct {
 // +kcc:proto=google.cloud.networkservices.v1.ExtensionChain.MatchCondition
 type ExtensionChain_MatchCondition struct {
 	// Required. A Common Expression Language (CEL) expression that is used to
-	//  match requests for which the extension chain is executed.
+	// match requests for which the extension chain is executed.
 	//
-	//  For more information, see [CEL matcher language
-	//  reference](https://cloud.google.com/service-extensions/docs/cel-matcher-language-reference).
+	// For more information, see [CEL matcher language
+	// reference](https://cloud.google.com/service-extensions/docs/cel-matcher-language-reference).
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.MatchCondition.cel_expression
 	// +required
 	CelExpression *string `json:"celExpression,omitempty"`
@@ -133,126 +133,125 @@ type ExtensionChain_MatchCondition struct {
 // +kubebuilder:validation:XValidation:rule="has(self.backendServiceRef) != has(self.wasmPluginRef)",message="either backendServiceRef or wasmPluginRef must be set"
 type ExtensionChain_Extension struct {
 	// Required. The name for this extension.
-	//  The name is logged as part of the HTTP request logs.
-	//  The name must conform with RFC-1034, is restricted to lower-cased
-	//  letters, numbers and hyphens, and can have a maximum length of 63
-	//  characters. Additionally, the first character must be a letter and the
-	//  last a letter or a number.
+	// The name is logged as part of the HTTP request logs.
+	// The name must conform with RFC-1034, is restricted to lower-cased
+	// letters, numbers and hyphens, and can have a maximum length of 63
+	// characters. Additionally, the first character must be a letter and the
+	// last a letter or a number.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.Extension.name
 	// +required
 	Name *string `json:"name,omitempty"`
 
 	// Optional. The `:authority` header in the gRPC request sent from Envoy
-	//  to the extension service.
-	//  Required for Callout extensions.
+	// to the extension service.
+	// Required for Callout extensions.
 	//
-	//  This field is not supported for plugin extensions. Setting it results in
-	//  a validation error.
+	// This field is not supported for plugin extensions. Setting it results in
+	// a validation error.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.Extension.authority
 	Authority *string `json:"authority,omitempty"`
 
 	// Optional. The reference to the service that runs the extension.
 	//
-	//  To configure a callout extension, `service` must be a fully-qualified
-	//  reference
-	//  to a [backend
-	//  service](https://cloud.google.com/compute/docs/reference/rest/v1/backendServices)
-	//  in the format:
-	//  `https://www.googleapis.com/compute/v1/projects/{project}/regions/{region}/backendServices/{backendService}`
-	//  or
-	//  `https://www.googleapis.com/compute/v1/projects/{project}/global/backendServices/{backendService}`.
+	// To configure a callout extension, `service` must be a fully-qualified reference
+	// to a [backend service](https://cloud.google.com/compute/docs/reference/rest/v1/backendServices)
+	// in the format:
+	// `https://www.googleapis.com/compute/v1/projects/{project}/regions/{region}/backendServices/{backendService}`
+	// or
+	// `https://www.googleapis.com/compute/v1/projects/{project}/global/backendServices/{backendService}`.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.Extension.service
 	BackendServiceRef *computev1beta1.ComputeBackendServiceRef `json:"backendServiceRef,omitempty"`
 
 	// Optional. The reference to the WasmPlugin resource.
 	//
-	//  To configure a plugin extension, `service` must be a reference
-	//  to a [`WasmPlugin`
-	//  resource](https://cloud.google.com/service-extensions/docs/reference/rest/v1beta1/projects.locations.wasmPlugins)
-	//  in the format:
-	//  `projects/{project}/locations/{location}/wasmPlugins/{plugin}`
-	//  or
-	//  `//networkservices.googleapis.com/projects/{project}/locations/{location}/wasmPlugins/{wasmPlugin}`.
+	// To configure a plugin extension, `service` must be a reference to a
+	// [`WasmPlugin` resource](https://cloud.google.com/service-extensions/docs/reference/rest/v1beta1/projects.locations.wasmPlugins)
+	// in the format:
+	// `projects/{project}/locations/{location}/wasmPlugins/{plugin}`
+	// or
+	// `//networkservices.googleapis.com/projects/{project}/locations/{location}/wasmPlugins/{wasmPlugin}`.
 	//
-	//  Plugin extensions are currently supported for the
-	//  `LBTrafficExtension`, the `LBRouteExtension`, and the `LBEdgeExtension`
-	//  resources.
+	// Plugin extensions are currently supported for the `LBTrafficExtension`, the
+	// `LBRouteExtension`, and the `LBEdgeExtension` resources.
+	//
+	// Note: Only the `external` subfield is supported as the
+	// `NetworkServicesWasmPlugin` resource is not yet supported in Config Connector.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.Extension.service
 	WasmPluginRef *NetworkServicesWasmPluginRef `json:"wasmPluginRef,omitempty"`
 
 	// Optional. A set of events during request or response processing for which
-	//  this extension is called.
+	// this extension is called.
 	//
-	//  For the `LBTrafficExtension` resource, this field is required.
+	// For the `LBTrafficExtension` resource, this field is required.
 	//
-	//  For the `LBRouteExtension` resource, this field is optional. If
-	//  unspecified, `REQUEST_HEADERS` event is assumed as supported.
+	// For the `LBRouteExtension` resource, this field is optional. If
+	// unspecified, `REQUEST_HEADERS` event is assumed as supported.
 	//
-	//  For the `LBEdgeExtension` resource, this field is required and must only
-	//  contain `REQUEST_HEADERS` event.
+	// For the `LBEdgeExtension` resource, this field is required and must only
+	// contain `REQUEST_HEADERS` event.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.Extension.supported_events
 	SupportedEvents []string `json:"supportedEvents,omitempty"`
 
 	// Optional. Specifies the timeout for each individual message on the
-	//  stream. The timeout must be between `10`-`10000` milliseconds. Required
-	//  for callout extensions.
+	// stream. The timeout must be between `10`-`10000` milliseconds. Required
+	// for callout extensions.
 	//
-	//  This field is not supported for plugin extensions. Setting it results in
-	//  a validation error.
+	// This field is not supported for plugin extensions. Setting it results in
+	// a validation error.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.Extension.timeout
 	Timeout *string `json:"timeout,omitempty"`
 
 	// Optional. Determines how the proxy behaves if the call to the extension
-	//  fails or times out.
+	// fails or times out.
 	//
-	//  When set to `TRUE`, request or response processing continues without
-	//  error. Any subsequent extensions in the extension chain are also
-	//  executed. When set to `FALSE` or the default setting of `FALSE` is used,
-	//  one of the following happens:
+	// When set to `TRUE`, request or response processing continues without
+	// error. Any subsequent extensions in the extension chain are also
+	// executed. When set to `FALSE` or the default setting of `FALSE` is used,
+	// one of the following happens:
 	//
-	//  * If response headers have not been delivered to the downstream client,
-	//  a generic 500 error is returned to the client. The error response can be
-	//  tailored by configuring a custom error response in the load balancer.
+	// * If response headers have not been delivered to the downstream client,
+	// a generic 500 error is returned to the client. The error response can be
+	// tailored by configuring a custom error response in the load balancer.
 	//
-	//  * If response headers have been delivered, then the HTTP stream to the
-	//  downstream client is reset.
+	// * If response headers have been delivered, then the HTTP stream to the
+	// downstream client is reset.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.Extension.fail_open
 	FailOpen *bool `json:"failOpen,omitempty"`
 
 	// Optional. List of the HTTP headers to forward to the extension
-	//  (from the client or backend). If omitted, all headers are sent.
-	//  Each element is a string indicating the header name.
+	// (from the client or backend). If omitted, all headers are sent.
+	// Each element is a string indicating the header name.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.Extension.forward_headers
 	ForwardHeaders []string `json:"forwardHeaders,omitempty"`
 
 	// Optional. The metadata provided here is included as part of the
-	//  `metadata_context` (of type `google.protobuf.Struct`) in the
-	//  `ProcessingRequest` message sent to the extension server.
+	// `metadata_context` (of type `google.protobuf.Struct`) in the
+	// `ProcessingRequest` message sent to the extension server.
 	//
-	//  The metadata is available under the namespace
-	//  `com.google.<extension_type>.<resource_name>.<extension_chain_name>.<extension_name>`.
-	//  For example:
-	//  `com.google.lb_traffic_extension.lbtrafficextension1.chain1.ext1`.
+	// The metadata is available under the namespace
+	// `com.google.<extension_type>.<resource_name>.<extension_chain_name>.<extension_name>`.
+	// For example:
+	// `com.google.lb_traffic_extension.lbtrafficextension1.chain1.ext1`.
 	//
-	//  The following variables are supported in the metadata:
+	// The following variables are supported in the metadata:
 	//
-	//  `{forwarding_rule_id}` - substituted with the forwarding rule's fully
-	//    qualified resource name.
+	// `{forwarding_rule_id}` - substituted with the forwarding rule's fully
+	// qualified resource name.
 	//
-	//  This field must not be set for plugin extensions. Setting it results in
-	//  a validation error.
+	// This field must not be set for plugin extensions. Setting it results in
+	// a validation error.
 	//
-	//  You can set metadata at either the resource level or the extension level.
-	//  The extension level metadata is recommended because you can pass a
-	//  different set of metadata through each extension to the backend.
+	// You can set metadata at either the resource level or the extension level.
+	// The extension level metadata is recommended because you can pass a
+	// different set of metadata through each extension to the backend.
 	//
-	//  This field is subject to following limitations:
+	// This field is subject to following limitations:
 	//
-	//  * The total size of the metadata must be less than 1KiB.
-	//  * The total number of keys in the metadata must be less than 16.
-	//  * The length of each key must be less than 64 characters.
-	//  * The length of each value must be less than 1024 characters.
-	//  * All values must be strings.
+	// * The total size of the metadata must be less than 1KiB.
+	// * The total number of keys in the metadata must be less than 16.
+	// * The length of each key must be less than 64 characters.
+	// * The length of each value must be less than 1024 characters.
+	// * All values must be strings.
 	// +kcc:proto:field=google.cloud.networkservices.v1.ExtensionChain.Extension.metadata
 	Metadata *apiextensionsv1.JSON `json:"metadata,omitempty"`
 }
